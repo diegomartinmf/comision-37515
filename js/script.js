ec2e9ac19064f57e0a370d5c46f27e59dfc41ede
kachelund <kachelund@gmail.com>
2022-07-18 22:52:50 -0300

    Se modifica el proyecto

--- js/script.js
+++ js/script.js
@@ -36,3 +36,32 @@ do {
         }
     }
 } while ((apellido == ""));
+
+let nombrecompleto;
+
+if ((nombre != null) && (apellido != null)) {
+    nombrecompleto = "A"
+} else if ((nombre != null) && (apellido == null)) {
+    nombrecompleto = "B"
+} else if ((nombre == null) && (apellido != null)) {
+    nombrecompleto = "C"
+} else { nombrecompleto = "D" }
+
+
+switch (nombrecompleto) {
+    case "A":
+        alert("Su nombre completo es: " + nombre + " " + apellido);
+        break;
+
+    case "B":
+        alert("Solo puso nombre: " + nombre);
+        break;
+
+    case "C":
+        alert("Solo puso apellido: " + apellido);
+        break;
+
+    case "D":
+        alert("No puso ni nombre ni apellido");
+        break;
+}
